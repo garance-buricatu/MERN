ad768a283f13727f8759937e958d6310d91a1524
Garance Buricatu <garance.mary@gmail.com>
2020-12-23 14:08:09 -0500

    Get Current User Profile

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => res.send('API Running')); //endpount to test
 // Define and access routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profiles'));
+app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 
