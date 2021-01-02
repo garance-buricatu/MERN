f036a6b89410f8c869c051add5b2fb02e9e8aa67
Garance Buricatu <garance.mary@gmail.com>
2021-01-02 13:42:06 -0500

    Posts & Comments

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,8 @@ import EditProfile from './components/profile-form/EditProfile';
 import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -51,6 +53,9 @@ const App = () => {
             <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
             <PrivateRoute exact path="/add-experience" component={ AddExperience } />
             <PrivateRoute exact path="/add-education" component={ AddEducation } />
+            <PrivateRoute exact path="/profile/:id" component={ Profile } />
+            <PrivateRoute exact path="/posts" component={ Posts } />
+
           </Switch>
         </section>
       </Fragment>
