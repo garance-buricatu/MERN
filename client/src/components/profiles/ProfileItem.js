41033a9a48655afc3dc091ebe9a93218eaa78ff8
Garance Buricatu <garance.mary@gmail.com>
2021-02-02 20:44:05 -0500

    bug fix

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 const ProfileItem = ({profile: { 
-    user : { _id, name, avatar},
+    user,
     status,
     company,
     location,
@@ -12,15 +12,17 @@ const ProfileItem = ({profile: {
 }) => {
     return (
         <div className="profile bg-light">
-            <img className="round-img" src={avatar} alt="" />
-            <duv>
-                <h2>{name}</h2>
-                <p>{status} {company && <span> at {company}</span>}</p>
-                <p className="my-1">{location && <span>{location}</span>}</p>
-                <Link to={`profile/${_id}`} className="btn btn-primary">
-                    View Profile
-                </Link>
-            </duv>
+            {user != null && 
+                <div>
+                    <img className="round-img" src={user.avatar} alt="" />
+                    <h2>{user.name}</h2>
+                    <p>{status} {company && <span> at {company}</span>}</p>
+                    <p className="my-1">{location && <span>{location}</span>}</p>
+                    <Link to={`profile/${user._id}`} className="btn btn-primary">
+                        View Profile
+                    </Link>
+                </div>
+            }
             <ul>
                 {skills.slice(0,4).map((skill, index) => (
                     <li key={index} className="text-primary">
